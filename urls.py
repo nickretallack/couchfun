3f482f98349e631e78d06c02a893a5c51b6b1dd2
Nick Retallack <nickretallack@gmail.com>
2008-08-19 00:56:47 -0700

    mega restructuring and new utilities

--- urls.py
+++ urls.py
@@ -13,19 +13,22 @@ web.webapi.internalerror = web.debugerror   # enables debugger
 
 urls = ('/','index','/(.*)','offer')
 
-offers_per_page = 10000
+offers_per_page = 100
 
 class index:
   def GET(self):
     page = int(web.input(page=0)['page'])
     h = Http()
-    response,content = h.request('%s/_view/offers/index?count=%d&skip=%d' % (database_url, offers_per_page, offers_per_page*page))
+    response,content = h.request('%s/_view/offers/index?count=8' % database_url)
+    print response,content
+    return 
     stuff = json.loads(content)    
     last_page = False
-    if stuff['total_rows'] - page*offers_per_page < offers_per_page:
+    if len(stuff['rows']) - page*offers_per_page < offers_per_page:
       last_page = True
     return render('index.html',{'offers':stuff['rows'],'page':page,'last_page':last_page})
 
 
 if __name__ == "__main__":
   web.run(urls, globals(), web.reloader)
+
